8e5efd1f818d97c17f704b534d5b46190c010274
sarmijavier <hjsarmiento1999@gmail.com>
2020-08-03 00:02:17 -0500

    Preparing webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path') // a donde vamos acceder es decir las carpetas
+const HtmlWebpackPlugin = require('html-webpack-plugin') // necesario para trabajar con HTLM
+
+//módulo de coniguración
+module.exports = {
+    entry: '.src/index.js ', // punto de entrada
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    //extensiones
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        //reglas -> aquí va babel
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    //plugins
+    plugins: [
+        new HtmlWebpackPlugin([
+            {
+                inject: true,
+                template: '.public/index.html',
+                filename: './index.html',
+            }
+        ])
+    ]
+}
